90b1caa62ed8d2c0accbf0e32bf5257d533ed58a
Shai <smatsri@gmail.com>
2022-12-20 22:19:52 +0200

    123

--- src/calendar/components/year/YearView.tsx
+++ src/calendar/components/year/YearView.tsx
@@ -1,26 +1,40 @@
 import { useCalendarContext } from "../../Context";
 import MonthView from "../shared/month/MonthView";
 import { useYearView } from "./hooks/useYeayView";
-import { YearViewContainer } from "./styles";
+import { DayViewContainer, HeaderContainer, MonthsContrainer, YearViewContainer } from "./styles";
 
 const YearView = () => {
-  const {selected:[year]} = useCalendarContext();
+  const { selected: [year], show, setSelected } = useCalendarContext();
   const { months } = useYearView(year);
+
+  const select = (month: number) => {
+    setSelected([year, month])
+    show('month')
+  }
+
+  console.log(year)
 
   return (
     <YearViewContainer>
-      {months.map(month => (
-        <MonthView
-          key={year + '-' + month}
-          year={year}
-          month={month}
-          DayView={({ day }) => (
-            <div key={day.day + '-' + day.month + '-' + day.year}>
-              {day.day}
-            </div>
-          )}
-        />
-      ))}
+      <HeaderContainer>
+        {year}
+      </HeaderContainer>
+      <MonthsContrainer>
+        {months.map(month => (
+          <div onClick={() => select(month)}>
+            <MonthView
+              key={year + '-' + month}
+              year={year}
+              month={month}
+              DayView={({ day }) => (
+                <DayViewContainer key={day.day + '-' + day.month + '-' + day.year}>
+                  {day.day}
+                </DayViewContainer>
+              )}
+            />
+          </div>
+        ))}
+      </MonthsContrainer>
     </YearViewContainer>
   );
 }
--- src/calendar/components/year/styles.tsx
+++ src/calendar/components/year/styles.tsx
@@ -2,10 +2,21 @@ import styled from "styled-components";
 
 export const YearViewContainer = styled.div`
   height:100%;
+  
+`
+
+export const HeaderContainer = styled.div`
+
+`
+
+export const MonthsContrainer = styled.div`
   display: grid;
   grid-template-columns: repeat(3, 1fr);
   grid-gap: 10px;
   > div {
     cursor: pointer;
   }
+`
+
+export const DayViewContainer = styled.div`
 `
